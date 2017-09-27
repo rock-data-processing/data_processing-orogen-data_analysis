60775ccb8fe0913663163593d0aca0e5610eff6a
Dennis Mronga <dennis.mronga@dfki.de>
2017-09-27 13:23:52 +0200

    Added weighted sum task and test script

---
+++ tasks/WeightedSum.cpp
@@ -0,0 +1,78 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "WeightedSum.hpp"
+
+using namespace data_analysis;
+
+WeightedSum::WeightedSum(std::string const& name, TaskCore::TaskState initial_state)
+    : WeightedSumBase(name, initial_state){
+}
+
+WeightedSum::WeightedSum(std::string const& name, RTT::ExecutionEngine* engine, TaskCore::TaskState initial_state)
+    : WeightedSumBase(name, engine, initial_state){
+}
+
+WeightedSum::~WeightedSum(){
+}
+
+bool WeightedSum::configureHook()
+{
+    if (! WeightedSumBase::configureHook())
+        return false;
+
+    weights = _weights.get();
+    for(int i = 0; i <  weights.size(); i++){
+        PortInterface *pi = new PortInterface(i, this);
+        input_ports.push_back(pi);
+    }
+    return true;
+}
+
+bool WeightedSum::startHook(){
+    if (! WeightedSumBase::startHook())
+        return false;
+    return true;
+}
+
+void WeightedSum::updateHook(){
+    WeightedSumBase::updateHook();
+
+    for(size_t i = 0; i < input_ports.size(); i++){
+        PortInterface* pi = input_ports[i];
+        if(!pi->new_data){
+            if(pi->port->read(pi->value) == RTT::NewData){
+                pi->new_data = true;
+            }
+        }
+    }
+
+    bool all_data = true;
+    for(size_t i = 0; i < input_ports.size(); i++)
+        all_data = all_data && input_ports[i]->new_data;
+
+    if(all_data){
+        double weighted_sum = 0;
+        std::cout<<"Data: "<<input_ports[0]->value<<" "<<input_ports[1]->value<<std::endl;
+        for(size_t i = 0; i < input_ports.size(); i++){
+            weighted_sum += weights(i) * input_ports[i]->value;
+            input_ports[i]->new_data = false;
+        }
+        _weighted_sum.write(weighted_sum);
+    }
+}
+
+void WeightedSum::errorHook(){
+    WeightedSumBase::errorHook();
+}
+
+void WeightedSum::stopHook(){
+    WeightedSumBase::stopHook();
+}
+
+void WeightedSum::cleanupHook(){
+    WeightedSumBase::cleanupHook();
+
+     for(size_t i = 0; i < input_ports.size(); i++)
+        delete input_ports[i];
+    input_ports.clear();
+}
---
+++ test/test_weighted_sum.rb
@@ -0,0 +1,28 @@
+require "orocos"
+Orocos.initialize
+
+Orocos.run "data_analysis::WeightedSum" => "weighted_sum" do
+    task = Orocos::TaskContext.get "weighted_sum"
+    weights = [0.5,0.5]
+    task.weights = Types.base.VectorXd.from_a weights
+    task.configure
+    task.start
+
+    writer_0 = task.input_0.writer
+    writer_1 = task.input_1.writer
+    reader = task.weighted_sum.reader
+
+    while true
+        input_data = [rand(), rand()]
+        writer_0.write input_data[0]
+        writer_1.write input_data[1]
+        sample = reader.read
+        if sample
+            puts "Input data: " + input_data.to_s
+            puts "Expected output: " + (weights[0] * input_data[0] + weights[1] * input_data[1]).to_s
+            puts "Output data: " + sample.to_s
+            puts "................................."
+        end
+        sleep 1
+    end
+end
