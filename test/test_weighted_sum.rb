e37d1f6e085ce8a97917a0d333070938e0dcb8ef
dmronga <dennis.mronga@dfki.de>
2018-01-11 21:42:40 +0100

    Update test scripts

--- test/test_weighted_sum.rb
+++ test/test_weighted_sum.rb
@@ -4,50 +4,35 @@ Orocos.conf.load_dir("config")
 
 Orocos.run "data_analysis::WeightedSumTask" => "weighted_sum" do
     task = Orocos::TaskContext.get "weighted_sum"
-    task.weights = Types.base.VectorXd.from_a [1,2]
+    Orocos.conf.apply(task, ["default", "rbs"])
     task.configure
     task.start
 
-    writer_0 = task.summand_0.writer
-    writer_1 = task.summand_1.writer
+    writer_1 = task.rbs_1.writer
+    writer_2 = task.rbs_2.writer
 
-    writer_2 = task.vect_summand_0.writer
-    writer_3 = task.vect_summand_1.writer
+    input_1 = Types.base.samples.RigidBodyState.new
+    input_2 = Types.base.samples.RigidBodyState.new
+    input_1.position = Types.base.Vector3d.new(1,2,3)
+    input_2.position = Types.base.Vector3d.new(4,5,6)
 
-    input_0 = 1
-    input_1 = 2
-    input_2 = Types.base.VectorXd.from_a [1,2]
-    input_3 = Types.base.VectorXd.from_a [3,4]
-
-    reader_0 = task.weighted_sum.reader
-    reader_1 = task.weighted_sum_vect.reader
+    reader = task.weighted_sum.reader
 
     puts "Press Ctrl-C to exit"
 
     while true
-        writer_0.write input_0
+        input_1.time = Types.base.Time.now
+        input_2.time = Types.base.Time.now
         writer_1.write input_1
+        writer_2.write input_2
         sleep 1
-        sample = reader_0.read
+        sample = reader.read
         if sample
-            puts "Input data 1:    " + input_0.to_s
-            puts "Input data 2:    " + input_1.to_s
+            puts "Input data 1:    " + input_1.position.to_a.to_s
+            puts "Input data 2:    " + input_2.position.to_a.to_s
             puts "Weights:         " + task.weights.to_s
-            puts "Output data:     " + sample.to_s
+            puts "Weighted sum:    " + sample.to_s
             puts
         end
-
-        writer_2.write input_2
-        writer_3.write input_3
-        sleep 0.5
-        sample = reader_1.read
-        if sample
-            puts "Input vector data 1:    " + input_2.to_s
-            puts "Input vector data 2:    " + input_3.to_s
-            puts "Weights:         " + task.weights.to_s
-            puts "Output data:     " + sample.to_s
-            puts "................................."
-        end
-        sleep 1
     end
 end
